d369434b3cc796fcb33b502f7d2c7f5b26808b83
fateful <vladislavvladislavovich@gmail.com>
2021-01-08 02:52:42 +0300

    Added explosive actor base class and implemented basic material change and explosion vfx when hp reaches 0

---
+++ Source/CoopGame/Private/LevelObjects/SExplosiveActor.cpp
@@ -0,0 +1,70 @@
+﻿#include "LevelObjects/SExplosiveActor.h"
+
+
+#include "CoopGame/CoopGame.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Player/Components/SHealthComponent.h"
+
+
+ASExplosiveActor::ASExplosiveActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMesh->SetCollisionObjectType(ECC_Destructible);
+	StaticMesh->SetCollisionResponseToAllChannels(ECR_Ignore);
+	StaticMesh->SetCollisionResponseToChannel(ECC_PhysicsBody, ECR_Block);
+	StaticMesh->SetCollisionResponseToChannel(ECC_Pawn, ECR_Block);
+	StaticMesh->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Block);
+	StaticMesh->SetCollisionResponseToChannel(ECC_WorldDynamic, ECR_Block);
+	StaticMesh->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+	StaticMesh->SetCollisionResponseToChannel(ECC_Camera, ECR_Block);
+	StaticMesh->SetCollisionResponseToChannel(ECC_Destructible, ECR_Block);
+	StaticMesh->SetCollisionResponseToChannel(ECC_WeaponTraceChannel, ECR_Block);
+	RootComponent = StaticMesh;
+	
+	HealthComp = CreateDefaultSubobject<USHealthComponent>(TEXT("Health"));
+}
+
+
+void ASExplosiveActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	HealthComp->OnHealthChanged.AddDynamic(this, &ASExplosiveActor::OnHealthChanged);
+}
+
+void ASExplosiveActor::OnHealthChanged(USHealthComponent* _, int32 HealthDelta)
+{
+	if (HealthComp->GetCurrentHealthPoints() <= 0 && !bDied)
+	{	
+		Explode();
+		
+		bDied = true;
+	}
+}
+
+void ASExplosiveActor::PlayCosmeticExplosionEffects()
+{
+	if (ExplodedMaterial)
+	{
+		StaticMesh->SetMaterial(0, ExplodedMaterial);
+	}
+
+	if (ExplosionEffect)
+	{
+		const FRotator RandRotator = UKismetMathLibrary::RandomRotator(false);
+		UGameplayStatics::SpawnEmitterAtLocation(this, ExplosionEffect, GetActorLocation(), RandRotator);
+	}
+}
+
+void ASExplosiveActor::Explode()
+{
+	if (!ensureAlways(!bDied))
+		return;
+
+	PlayCosmeticExplosionEffects();
+
+	UE_LOG(LogTemp, Warning, TEXT("%s: Exploded!"));
+}
---
+++ Source/CoopGame/Public/LevelObjects/SExplosiveActor.h
@@ -0,0 +1,43 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SExplosiveActor.generated.h"
+
+class USHealthComponent;
+UCLASS()
+class COOPGAME_API ASExplosiveActor : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, Category=Mesh)
+	UStaticMeshComponent* StaticMesh;
+	
+	UPROPERTY(EditDefaultsOnly, Category=Mesh)
+	UMaterial* ExplodedMaterial;
+	
+	UPROPERTY(EditDefaultsOnly, Category=Explosion)
+	UParticleSystem* ExplosionEffect;
+	
+	UPROPERTY(VisibleDefaultsOnly, Category=Health)
+	USHealthComponent* HealthComp;
+
+private:
+	bool bDied = false;
+	
+public:
+	ASExplosiveActor();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UFUNCTION()
+	void OnHealthChanged(USHealthComponent* _, int32 HealthDelta);
+	void PlayCosmeticExplosionEffects();
+
+	void Explode();
+};
