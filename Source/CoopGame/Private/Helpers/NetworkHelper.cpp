b22e14f7c9a2d416a33aed0b71aa4648911b0ad6
fateful <vladislavvladislavovich@gmail.com>
2021-01-08 17:56:39 +0300

    Added helper function for locally controlled and for autonomous proxy detection

--- Source/CoopGame/Private/Helpers/NetworkHelper.cpp
+++ Source/CoopGame/Private/Helpers/NetworkHelper.cpp
@@ -31,3 +31,61 @@ bool FNetworkHelper::HasCosmetics(const UActorComponent* ActorComponent)
 
 	return false;
 }
+
+bool FNetworkHelper::IsLocallyControlled(const APawn* Pawn)
+{
+	if (Pawn)
+		return Pawn->IsLocallyControlled();
+
+	return false;
+}
+
+bool FNetworkHelper::IsLocallyControlled(const AActor* Actor)
+{
+	if (Actor)
+	{
+		if (const APawn* Pawn = Cast<APawn>(Actor))
+			return IsLocallyControlled(Pawn);
+		else if (const APawn* PawnOwner = Cast<APawn>(Actor->GetOwner()))
+			return IsLocallyControlled(PawnOwner);
+	}
+
+	return false;
+}
+
+bool FNetworkHelper::IsLocallyControlled(const UActorComponent* ActorComponent)
+{
+	if (ActorComponent)
+		return IsLocallyControlled(ActorComponent->GetOwner());
+
+	return false;
+}
+
+bool FNetworkHelper::IsAutonomousProxy(const APawn* Pawn)
+{
+	if (Pawn)
+		return Pawn->GetLocalRole() == ROLE_AutonomousProxy;
+
+	return false;
+}
+
+bool FNetworkHelper::IsAutonomousProxy(const AActor* Actor)
+{
+	if (Actor)
+	{
+		if (const APawn* Pawn = Cast<APawn>(Actor))
+			return IsAutonomousProxy(Pawn);
+		else if (const APawn* PawnOwner = Cast<APawn>(Actor->GetOwner()))
+			return IsAutonomousProxy(PawnOwner);
+	}
+	
+	return false;
+}
+
+bool FNetworkHelper::IsAutonomousProxy(const UActorComponent* ActorComponent)
+{
+	if (ActorComponent)
+		return IsAutonomousProxy(ActorComponent->GetOwner());
+
+	return false;
+}
--- Source/CoopGame/Public/Helpers/NetworkHelper.h
+++ Source/CoopGame/Public/Helpers/NetworkHelper.h
@@ -6,5 +6,13 @@ struct FNetworkHelper
 	static bool HasAuthority(const UActorComponent* ActorComponent);
 
 	static bool HasCosmetics(const AActor* Actor);
-	static bool HasCosmetics(const UActorComponent* ActorComponent);	
+	static bool HasCosmetics(const UActorComponent* ActorComponent);
+
+	static bool IsLocallyControlled(const APawn* Pawn);
+	static bool IsLocallyControlled(const AActor* Actor);
+	static bool IsLocallyControlled(const UActorComponent* ActorComponent);
+
+	static bool IsAutonomousProxy(const APawn* Pawn);
+	static bool IsAutonomousProxy(const AActor* Actor);
+	static bool IsAutonomousProxy(const UActorComponent* ActorComponent);
 };
