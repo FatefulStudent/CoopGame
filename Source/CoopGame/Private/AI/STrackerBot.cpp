11b2fe30dc312771617a7364f57a005f8ad71878
Vlad Ivanov <v_ivanov@wargaming.net>
2021-01-11 23:20:17 +0300

    Added bare pawn with static mesh and material for it

---
+++ Source/CoopGame/Private/AI/STrackerBot.cpp
@@ -0,0 +1,22 @@
+﻿#include "AI/STrackerBot.h"
+
+
+ASTrackerBot::ASTrackerBot()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMeshComp->SetCanEverAffectNavigation(false);
+	RootComponent = StaticMeshComp;
+}
+
+void ASTrackerBot::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASTrackerBot::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Source/CoopGame/Public/AI/STrackerBot.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "STrackerBot.generated.h"
+
+UCLASS()
+class COOPGAME_API ASTrackerBot : public APawn
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, Category=Visual)
+	UStaticMeshComponent* StaticMeshComp;
+
+	
+
+public:
+	ASTrackerBot();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
