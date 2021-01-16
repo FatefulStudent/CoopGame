ad12a5627bbdc1c6d7df5bd319b36ebb3b498426
Vlad Ivanov <v_ivanov@wargaming.net>
2021-01-16 18:24:02 +0300

    Added movement logic to tracker bot

--- Source/CoopGame/Private/AI/STrackerBot.cpp
+++ Source/CoopGame/Private/AI/STrackerBot.cpp
@@ -1,22 +1,64 @@
 ﻿#include "AI/STrackerBot.h"
 
 
+#include "NavigationPath.h"
+#include "NavigationSystem.h"
+#include "GameFramework/Character.h"
+#include "Kismet/GameplayStatics.h"
+
+
 ASTrackerBot::ASTrackerBot()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
 	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
 	StaticMeshComp->SetCanEverAffectNavigation(false);
+	StaticMeshComp->SetSimulatePhysics(true);
 	RootComponent = StaticMeshComp;
 }
 
 void ASTrackerBot::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	// Initial move-to
+	NextPathPoint = CalculateNextPathPoint();
 }
 
 void ASTrackerBot::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	MoveToTargetByForce();
+}
+
+FVector ASTrackerBot::CalculateNextPathPoint() const
+{
+	AActor* GoalActor = UGameplayStatics::GetPlayerCharacter(this, 0);
+	if (!GoalActor)
+		return GetActorLocation();
+	 
+	UNavigationPath* NavPath = UNavigationSystemV1::FindPathToActorSynchronously(GetWorld(), GetActorLocation(), GoalActor);
+
+	if (NavPath->PathPoints.Num() > 1)
+	{
+		return NavPath->PathPoints[1];
+	}
+
+	return GetActorLocation();
+}
+
+void ASTrackerBot::MoveToTargetByForce()
+{
+	const bool bIsAtGoal = FVector::Distance(GetActorLocation(), NextPathPoint) < AcceptanceRadiusToTarget;
+	if (bIsAtGoal)
+	{
+		NextPathPoint = CalculateNextPathPoint();
+	}
+	else
+	{
+		const FVector ForceDirection = (NextPathPoint - GetActorLocation()).GetSafeNormal();
+		const FVector Force = ForceDirection * RollingForceStrength;
+		StaticMeshComp->AddForce(Force, NAME_None, bAccelChange);
+	}
 }
--- Source/CoopGame/Public/AI/STrackerBot.h
+++ Source/CoopGame/Public/AI/STrackerBot.h
@@ -13,14 +13,32 @@ protected:
 	UPROPERTY(VisibleDefaultsOnly, Category=Visual)
 	UStaticMeshComponent* StaticMeshComp;
 
-	
+	// How quickly it will move to next path point
+	UPROPERTY(EditDefaultsOnly, Category=Rolling)
+	float RollingForceStrength = 100.0f;
+
+	// When the target will be in acceptance radius movement will stop
+	UPROPERTY(EditDefaultsOnly, Category=Rolling)
+	float AcceptanceRadiusToTarget = 50.0f;
+
+	// If true, mass will have no effect on movement
+	UPROPERTY(EditDefaultsOnly, Category=Rolling)
+	bool bAccelChange = true;
+
+	// If true, mass will have no effect on movement
+	UPROPERTY(BlueprintReadOnly, Category=Rolling)
+	// Next point in navigation path to player character
+	FVector NextPathPoint = FVector(ForceInitToZero);
 
 public:
 	ASTrackerBot();
 
 protected:
 	virtual void BeginPlay() override;
-
-public:
 	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable)
+	FVector CalculateNextPathPoint() const;
+
+	void MoveToTargetByForce();
 };
